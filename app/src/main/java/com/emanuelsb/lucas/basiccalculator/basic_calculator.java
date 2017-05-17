fcdd106538708e036ae81d52a634d465e2026aa3
Lucas Emanuel <lucasemsb@hotmail.com>
2017-05-17 00:59:47 -0300

    Ultimate final version

--- app/src/main/java/com/emanuelsb/lucas/basiccalculator/basic_calculator.java
+++ app/src/main/java/com/emanuelsb/lucas/basiccalculator/basic_calculator.java
@@ -2,6 +2,7 @@ package com.emanuelsb.lucas.basiccalculator;
 
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
@@ -41,18 +42,10 @@ public class basic_calculator extends AppCompatActivity{
             @Override
             public void onClick(View v) {
 
-                if (campoOperando1.length() == 0){
+                if (campoOperando1.getText().toString().length() == 0){
                     Toast.makeText(getApplicationContext(), "Campo do operando 1 vazio",Toast.LENGTH_SHORT).show();
-                }
-                else if (campoOperando1.length() == 0) {
+                } else if (campoOperando2.getText().toString().length() == 0 ) {
                     Toast.makeText(getApplicationContext(), "Campo do operando 2 vazio",Toast.LENGTH_SHORT).show();
-
-                } else if (Double.parseDouble(campoOperando2.getText().toString()) == 0) {
-                    AlertDialog.Builder alerta = new AlertDialog.Builder(basic_calculator.this);
-                    alerta.setTitle("Erro");
-                    alerta.setIcon(R.drawable.error);
-                    alerta.setMessage("Impossível dividir por 0");
-                    alerta.show();
                 } else {
 
                     double valor1 = Double.parseDouble(campoOperando1.getText().toString());
@@ -74,10 +67,19 @@ public class basic_calculator extends AppCompatActivity{
                         alerta.setMessage(Double.toString(valor1 * valor2));
                         alerta.show();
                     } else if (divi.isChecked()) {
-                        AlertDialog.Builder alerta = new AlertDialog.Builder(basic_calculator.this);
-                        alerta.setTitle("Resposta: ");
-                        alerta.setMessage(Double.toString(valor1 / valor2));
-                        alerta.show();
+                        if( Double.parseDouble(campoOperando2.getText().toString()) != 0){
+                            AlertDialog.Builder alerta = new AlertDialog.Builder(basic_calculator.this);
+                            alerta.setTitle("Resposta: ");
+                            alerta.setMessage(Double.toString(valor1 / valor2));
+                            alerta.show();
+                        } else {
+                            AlertDialog.Builder alerta = new AlertDialog.Builder(basic_calculator.this);
+                            alerta.setTitle("Erro");
+                            alerta.setIcon(R.drawable.error);
+                            alerta.setMessage("Impossível dividir por 0");
+                            alerta.show();
+                        }
+
                     } else {
                         Toast.makeText(getApplicationContext(), "Nenhuma operação foi selecionada", Toast.LENGTH_SHORT).show();
 
@@ -89,5 +91,4 @@ public class basic_calculator extends AppCompatActivity{
 
     }
 
-    }
-
+}
